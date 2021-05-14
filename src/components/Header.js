f367f79241c1f3e4f44b0a1dcb676621a1ed557a
Roi malka <roimalka10@gmail.com>
2021-05-14 22:47:38 +0300

    made logo a home back button

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,9 @@ const Header = (props) => {
   return (
     <Nav>
       <Logo>
-        <img src="/images/logo.svg" alt="Disney" />
+        <a href="/home">
+          <img src="/images/logo.svg" alt="Disney" />
+        </a>
       </Logo>
 
       {!userName ? (
